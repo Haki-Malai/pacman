8ba79ab369c35d298abb1820f10f143ee640a9ff
Haki-Malai <p17mala@ionio.gr>
2021-06-10 21:54:54 +0300

    Added JavaScript, Phaser and assets

---
+++ code/javascript/main.js
@@ -0,0 +1,47 @@
+﻿var config = {
+    type: Phaser.WEBGL,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var controls;
+
+function preload ()
+{
+    this.load.image('tiles', '../../assets/tiles/tileset.png')
+    this.load.tilemapTiledJSON('map', '../../assets/tiles/tileset.json')
+}
+
+function create ()
+{
+    var map = this.make.tilemap({ key: 'map' })
+    var tiles = map.addTilesetImage('cybernoid', 'tiles', tileWidth=16, tileHeight=16)
+    var layer = map.createLayer(0, tiles, 0, 0)
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+
+    var cursors = this.input.keyboard.createCursorKeys()
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    }
+    controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig)
+}
+
+function update (time, delta)
+{
+    controls.update(delta)
+}
