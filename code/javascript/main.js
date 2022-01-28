1b1d1cdebbb7674a37a9b0fa4e1729f57813cad6
Haki-Malai <p17mala@ionio.gr>
2022-01-28 22:30:22 +0200

    Direction Switch and other minor details/improvements

--- code/javascript/main.js
+++ code/javascript/main.js
@@ -17,17 +17,28 @@
         this.layer = this.map.createLayer(0, this.tiles, 0, 0);
         this.levelData = this.cache.json.get('levelData');
         
-        this.pacman = this.physics.add.sprite(this.map.tileToWorldX(this.levelData.startsXAt)+10, this.map.tileToWorldY(this.levelData.startsYAt)+9.5, 'pacman');
+        this.pacman = this.physics.add.sprite(this.map.tileToWorldX(this.levelData.startsXAt)+10, this.map.tileToWorldY(this.levelData.startsYAt)+10, 'pacman');
         this.pacman.displayWidth = 10;
         this.pacman.displayHeight = 10.5;
-
-        this.points = this.physics.add.group({
-            key: "point"
+        this.pacman.moved = {
+            x : 0,
+            y : 0
+        }
+        this.pacman.direction = {
+            next : 'right',
+            current : 'right'
+        }
+        
+        // Animation set
+        this.anims.create({
+            key: 'eat',
+            frames: this.anims.generateFrameNumbers('pacman', { frames: [0, 1, 2, 3, 2, 1, 0]}),
+            frameRate: 18,
         });
-        this.points.enableBody = true;
+
+        this.points = this.physics.add.group();
 
         for (let i = 2; i <= 48; i++) {
-            var tempArray = [];
             for (let j = 2; j <=48; j++) {
                 if (i != 25 || j != 26) {
                     if (shouldAddPoint(this.layer.getTileAt(i, j))) {
@@ -41,20 +52,12 @@
 
         this.physics.add.overlap(this.pacman, this.points, eatPoint, null, this);
 
-        this.camera = this.camera = this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-        this.camera.zoomTo(5);
-        this.cameras.main.startFollow(this.pacman, true, 0.09, 0.09);
+        //this.camera = this.camera = this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        //this.camera.zoomTo(5);
+        //this.cameras.main.startFollow(this.pacman, true, 0.09, 0.09);
         
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.pacman.moved = {
-            x : 0,
-            y : 0
-        }
-        this.pacman.direction = {
-            next : 'right',
-            current : 'right'
-        }
 
         function shouldAddPoint(tile) {
             if ((tile.index != 15 || tile.rotation !=0) && (tile.index != 16 || tile.rotation !=0) && (tile.index != 24 || tile.rotation !=0) && (tile.index != 25 || tile.rotation !=0) && (tile.index != 26 || tile.rotation !=0) && (tile.index != 27 || tile.rotation !=0) && (tile.index != 28 || tile.rotation !=0)){
@@ -66,14 +69,31 @@
 
         function eatPoint(pacman, point) {
             point.disableBody(true, true);
+            this.pacman.play('eat');
         }
     }
     update(time, delta) {
+
+        console.log(this.pacman.angle)
+        // Rotates the PacMan
+        if (this.pacman.direction.current === 'right') {
+            this.pacman.angle = 0;
+            if (this.pacman.flipY) this.pacman.flipY = false; 
+        }else if (this.pacman.direction.current === 'left' && this.pacman.angle != -180) {
+            this.pacman.angle = 180;
+            if (!this.pacman.flipY) this.pacman.flipY = true; 
+        }
+        if (this.pacman.direction.current === 'up' && this.pacman.angle != -90) {
+            this.pacman.angle = -90;
+        }else if (this.pacman.direction.current === 'down' && this.pacman.angle != 90) {
+            this.pacman.angle = 90
+        }
+
         // After 16 moved it means that we traveled a full box
-        if (Math.abs(this.pacman.moved.y) == 16) {
+        if (Math.abs(this.pacman.moved.y) === 16) {
             this.pacman.moved.y = 0;
         } 
-        if (Math.abs(this.pacman.moved.x) == 16) {
+        if (Math.abs(this.pacman.moved.x) === 16) {
             this.pacman.moved.x = 0;
         }
         
@@ -103,13 +123,13 @@
         
         // Here we check if we can set the current direction same to the next
         if (this.pacman.direction.current != this.pacman.direction.next) {
-            if ((this.pacman.direction.next == 'left' || this.pacman.direction.next == 'right') && this.pacman.moved.x == 0 && this.pacman.moved.y == 0) {
+            if ((this.pacman.direction.next === 'left' || this.pacman.direction.next === 'right') && this.pacman.moved.x === 0 && this.pacman.moved.y === 0) {
                 if (canMove(this.pacman.direction.next, this.pacman.moved.y, this.pacman.moved.x, collisionTiles)) {
                     this.pacman.direction.current = this.pacman.direction.next;
                     this.pacman.moved.x = 0;
                 }
             }
-            else if (((this.pacman.direction.next == 'up') || (this.pacman.direction.next == 'down')) && (this.pacman.moved.y == 0) && (this.pacman.moved.x == 0)) {
+            else if (((this.pacman.direction.next === 'up') || (this.pacman.direction.next === 'down')) && (this.pacman.moved.y === 0) && (this.pacman.moved.x === 0)) {
                 if (canMove(this.pacman.direction.next, this.pacman.moved.y, this.pacman.moved.x, collisionTiles)) {
                     this.pacman.direction.current = this.pacman.direction.next
                     this.pacman.moved.y = 0
@@ -118,30 +138,30 @@
         }
         
         if (canMove(this.pacman.direction.current, this.pacman.moved.y, this.pacman.moved.x, collisionTiles)) {
-            if (this.pacman.direction.current == 'right') {
+            if (this.pacman.direction.current === 'right') {
                 this.pacman.x += 1;
                 this.pacman.moved.x += 1;
             }
-            else if (this.pacman.direction.current == 'left') {
+            else if (this.pacman.direction.current === 'left') {
                 this.pacman.x -= 1;
                 this.pacman.moved.x -= 1;
             }
-            else if (this.pacman.direction.current == 'up') {
+            else if (this.pacman.direction.current === 'up') {
                 this.pacman.y -= 1;
                 this.pacman.moved.y -= 1;
             }
-            else if (this.pacman.direction.current == 'down') {
+            else if (this.pacman.direction.current === 'down') {
                 this.pacman.y += 1;
                 this.pacman.moved.y += 1;
             }
         }
         
         function canMove(direction, movedY, movedX, collisionTiles) {
-            if (moving == false) {
+            if (moving === false) {
                 return false;
             }
-            if (direction == 'up') {
-                if (((collisionTiles.current == 1 || collisionTiles.current == 3 || collisionTiles.current == 4 || collisionTiles.current == 9 || collisionTiles.current == 18 || collisionTiles.current == 23) && collisionTiles.currentRotation == 0) || ((collisionTiles.current == 9 || collisionTiles.current == 10) && collisionTiles.currentRotation > 4)) {
+            if (direction === 'up') {
+                if (((collisionTiles.current === 1 || collisionTiles.current === 3 || collisionTiles.current === 4 || collisionTiles.current === 9 || collisionTiles.current === 18 || collisionTiles.current === 23) && collisionTiles.currentRotation === 0) || ((collisionTiles.current === 9 || collisionTiles.current === 10) && collisionTiles.currentRotation > 4)) {
                     if ((movedY >= -16) && (movedY < 0)) {
                         return true;
                     } else {
@@ -150,8 +170,8 @@
                 } else {
                     return true
                 }
-            } else if (direction == 'down') {
-                if (((collisionTiles.down == 3 || collisionTiles.down == 4 || collisionTiles.down == 15 || collisionTiles.down == 16 || collisionTiles.down == 18 || collisionTiles.down == 1 || collisionTiles.down == 24 || collisionTiles.down == 25 || collisionTiles.down == 26 || collisionTiles.down == 27 || collisionTiles.down == 28) && collisionTiles.downRotation == 0) || ((collisionTiles.down == 9 || collisionTiles.down == 10) && collisionTiles.downRotation > 4)) {
+            } else if (direction === 'down') {
+                if (((collisionTiles.down === 3 || collisionTiles.down === 4 || collisionTiles.down === 15 || collisionTiles.down === 16 || collisionTiles.down === 18 || collisionTiles.down === 1 || collisionTiles.down === 24 || collisionTiles.down === 25 || collisionTiles.down === 26 || collisionTiles.down === 27 || collisionTiles.down === 28) && collisionTiles.downRotation === 0) || ((collisionTiles.down === 9 || collisionTiles.down === 10) && collisionTiles.downRotation > 4)) {
                     if ((movedY <= 16) && (movedY > 0)) {
                         return true;
                     } else {
@@ -160,8 +180,8 @@
                 } else {
                     return true;
                 }
-            } else if (direction == 'right') {
-                if (((collisionTiles.right == 1 || collisionTiles.right == 15 || collisionTiles.right == 24) && collisionTiles.rightRotation == 0) || ((collisionTiles.right == 3 || collisionTiles.right == 4 || collisionTiles.right == 10 || collisionTiles.right == 18 || collisionTiles.right == 23) && collisionTiles.rightRotation > 4)) {
+            } else if (direction === 'right') {
+                if (((collisionTiles.right === 1 || collisionTiles.right === 15 || collisionTiles.right === 24) && collisionTiles.rightRotation === 0) || ((collisionTiles.right === 3 || collisionTiles.right === 4 || collisionTiles.right === 10 || collisionTiles.right === 18 || collisionTiles.right === 23) && collisionTiles.rightRotation > 4)) {
                     if ((movedX <= 16) && (movedX > 0)) {
                         return true;
                     } else {
@@ -170,8 +190,8 @@
                 } else {
                     return true;
                 }
-            } else if (direction == 'left') {
-                if (((collisionTiles.current == 1 || collisionTiles.current == 16 || collisionTiles.current == 28) && collisionTiles.currentRotation == 0) || ((collisionTiles.current == 3 || collisionTiles.current == 4 || collisionTiles.current == 10 || collisionTiles.current == 18) && collisionTiles.currentRotation > 4)) {
+            } else if (direction === 'left') {
+                if (((collisionTiles.current === 1 || collisionTiles.current === 16 || collisionTiles.current === 28) && collisionTiles.currentRotation === 0) || ((collisionTiles.current === 3 || collisionTiles.current == 4 || collisionTiles.current == 10 || collisionTiles.current == 18) && collisionTiles.currentRotation > 4)) {
                     if ((movedX >= -16) && (movedX < 0)) {
                         return true;
                     } else {
